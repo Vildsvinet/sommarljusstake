f030c89369f605f64ffaa725843a35add5bf60d1
Utbildadninja <34104553+Utbildadninja@users.noreply.github.com>
2022-03-29 16:32:30 +0200

    Update homeView.js
    Lagt till en off knapp för att testa om autodeply fungerar

--- webpage/public/homeView.js
+++ webpage/public/homeView.js
@@ -3,6 +3,8 @@ function HomeView(props){
         <div><center>
             <button>
                 press this button to turn on the sommarljusstake</button>
+				<button>
+                press this button to turn off the sommarljusstake</button>
         </center>
         </div>
     )
