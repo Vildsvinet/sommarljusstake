add93837173b23be0308e09e25aa97af9e7ed8bd
Vildsvinet <95482627+Vildsvinet@users.noreply.github.com>
2022-04-14 11:29:59 +0200

    updated views

--- webpage/public/homeView.js
+++ webpage/public/homeView.js
@@ -1,9 +1,18 @@
-function HomeView(props){
-    return(
-        <div><center>
-            <button  onClick = {event => window.location.hash = "#about"} >
-                press this button to turn on the Sommarljusstake</button>
-        </center>
+function HomeView(props) {
+    return (
+        <div>
+            <center>
+                <button>
+                    press this button to turn on the Sommarljusstake
+                </button>
+            </center>
+            <br/>
+
+            <center>
+                <button onClick={() => window.location.hash = "#about"}>
+                    press this button to read about the team
+                </button>
+            </center>
         </div>
     )
 }
