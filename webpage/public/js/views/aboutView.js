e2a333a2c613ff4743e213f7eb5d827b6bd1fc3b
Utbildadninja <34104553+Utbildadninja@users.noreply.github.com>
2022-05-03 13:27:47 +0200

    Update aboutView.js

--- webpage/public/js/views/aboutView.js
+++ webpage/public/js/views/aboutView.js
@@ -1,8 +1,14 @@
-function AboutView(props){
-    return(
-        <div><center>
-            wooo read about us
-        </center>
+function AboutView(props) {
+    return (
+        <div>
+            <center>
+                <img src={"team1.jpg"} width={"300px"} alt={"The team"}/><br/>
+                The team behind Sommarljusstake<br/>
+
+                <button onClick={() => window.location.hash = "#home"}>
+                    back to home
+                </button>
+            </center>
         </div>
     )
 }
