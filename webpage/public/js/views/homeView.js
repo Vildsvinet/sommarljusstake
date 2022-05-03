c403a361b99ab48b4774caf0cb5d88738b41e6e0
Utbildadninja <34104553+Utbildadninja@users.noreply.github.com>
2022-05-03 13:21:01 +0200

    Update homeView.js

--- webpage/public/js/views/homeView.js
+++ webpage/public/js/views/homeView.js
@@ -13,7 +13,7 @@ function HomeView(props){
 
     return <div>
         <center>
-            <button  onClick = {() => window.location.hash = "#about"}>About us</button>
+            <button onClick = {() => window.location.hash = "#about"}>About us</button>
             <button onClick = {() => props.setLight(true)}>ON</button>
             <button onClick = {() => props.setLight(false)}>OFF</button>
         </center>
