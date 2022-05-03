2e0093247d43b5de2388c404c2ba6227e45c6731
Utbildadninja <34104553+Utbildadninja@users.noreply.github.com>
2022-05-03 12:07:47 +0200

    Added several tests for the model interface

--- webpage/__tests__/Model.test.js
+++ webpage/__tests__/Model.test.js
@@ -7,12 +7,31 @@ test("Sanity", () => {
 describe("Model interface", () => {
     const name = new model();
 
+    test("defines Observer", () => {
+        expect(typeof name.observers).toBe("object");
+    })
+    test("Observer empty list", () => {
+        expect(name.observers).toEqual([])
+    })
+
+    test("defines currentUser", () => {
+        expect(typeof name.currentUser).toBe("object");
+    })
+    test("currentUser null", () => {
+        expect(name.currentUser).toBe(null)
+    })
+
+    test("defines currentUsername", () => {
+        expect(typeof name.currentUsername).toBe("object");
+    })
+    test("currentUsername null", () => {
+        expect(name.currentUsername).toBe(null)
+    })
+
     test("defines lightOn()", () => {
         expect(typeof name.lightOn).toBe("boolean");
     })
-
     test('Light off default', () => {
         expect(name.lightOn).toBe(false)
     });
 })
-
