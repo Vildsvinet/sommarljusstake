d58c6f3855eac77350b4177baceeb0ec2d3af20e
Tove <95482627+Vildsvinet@users.noreply.github.com>
2022-05-05 23:28:00 +0200

    some styling

--- webpage/public/js/views/homeView.js
+++ webpage/public/js/views/homeView.js
@@ -1,29 +1,23 @@
-function HomeView(props){
+function HomeView(props) {
     let sneaky;
     if (props.currentUser == null) {
         sneaky = ""
-    }
-    else sneaky = "hidden";
+    } else sneaky = "hidden";
 
     let reverseSneaky;
     if (!(props.currentUser == null)) {
         reverseSneaky = ""
-    }
-    else reverseSneaky = "hidden";
+    } else reverseSneaky = "hidden";
 
     return <div>
-        <center>
-            <button id={sneaky} className="" onClick={() => window.location.hash="#login"}>Sign in</button>
-            <button id={reverseSneaky} className="" onClick={() => window.location.hash = "#controls"}>Controls</button>
-            <br/>
-            <button id={reverseSneaky} className="" onClick={() => props.signOut()}>Sign out</button>
-            <br/>
-            <button onClick = {() => window.location.hash = "#about"}>About</button>
+        <img src={"midsommarljusstake.png"} alt={"logo"}/>
 
-        </center>
-
-
+        <button id={sneaky} className="" onClick={() => window.location.hash = "#login"}>sign in</button>
+        <button id={reverseSneaky} className="" onClick={() => window.location.hash = "#controls"}>controls</button>
+        <br/>
+        <button id={reverseSneaky} className="" onClick={() => props.signOut()}>sign out</button>
+        <br/>
+        <button onClick={() => window.location.hash = "#about"}>about</button>
 
     </div>
-
 }
