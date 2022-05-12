4d45b39ebd8830b8068e9d961a7e5686ec633fd2
Utbildadninja <34104553+Utbildadninja@users.noreply.github.com>
2022-05-12 15:59:34 +0200

    Added more complex tests

--- webpage/__tests__/Model.test.js
+++ webpage/__tests__/Model.test.js
@@ -34,6 +34,45 @@ describe("Model interface", () => {
     test('Light off default', () => {
         expect(name.lightOn).toBe(false)
     });
+
+    describe("setLight test", () => {
+        const model1 = new model();
+        const setLightSpy = jest.spyOn(model1, "setLight");
+
+        const trueRule = jest.fn(() => true);
+
+        describe(".setLight", () => {
+            test("defines a function", () => {
+                expect(typeof model1.setLight).toBe("function");
+            });
+
+            test("sets lightStatus when called", () => {
+                expect(model1.setLight(true)).toBeUndefined(); // Can be replaced with model1.setLight(true)
+                expect(setLightSpy).toHaveBeenLastCalledWith(true);
+
+                setLightSpy.mockClear();
+            });
+        });
+    });
+
+    describe("setDimmer test", () => {
+        const model1 = new model();
+        const setDimmerSpy = jest.spyOn(model1, "setDimmer");
+
+        describe(".setDimmer", () => {
+            test("defines a function", () => {
+                expect(typeof model1.setDimmer).toBe("function");
+            });
+
+            test("sets dimmer when called", () => {
+                expect(model1.setDimmer(10)).toBeUndefined();
+                expect(setDimmerSpy).toHaveBeenLastCalledWith(10);
+
+                setDimmerSpy.mockClear();
+            });
+        });
+    });
+
     //Tove below
     test("defines pendingMessage", () => {
             expect(typeof name.pendingMessage).toBe("boolean");
